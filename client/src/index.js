fe61ddbd5c1d70b546cafe67c6063d2631ea50d5
Brad Windsor <bradwindsor@Brads-MBP.fios-router.home>
2017-10-05 00:41:34 -0400

    Removing keys for commit

--- client/src/index.js
+++ client/src/index.js
@@ -7,10 +7,7 @@ import registerServiceWorker from './registerServiceWorker';
 var firebase = require("firebase");
 
 var config = {
-  apiKey: "AIzaSyAAUDsEqZ9_GGn3PwnukhaGj_L5zXTyKuw",
-  authDomain: "mythical-ember-168003.firebaseapp.com",
-  databaseURL: "https://mythical-ember-168003.firebaseio.com",
-  storageBucket: "mythical-ember-168003.appspot.com",
+
 };
 firebase.initializeApp(config);
 
